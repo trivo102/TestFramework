818ee2267519f1470fae953ab554f470b4dd01ab
Minh Tri <tri@wonolo.com>
2019-08-25 14:56:10 +0700

    Added Shared.framework

---
+++ Shared.framework/Headers/Shared.h
@@ -0,0 +1,178 @@
+#import <Foundation/Foundation.h>
+
+@class SharedUser, SharedVideoGame;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KotlinBase : NSObject
+- (instancetype)init __attribute__((unavailable));
++ (instancetype)new __attribute__((unavailable));
++ (void)initialize __attribute__((objc_requires_super));
+@end;
+
+@interface KotlinBase (KotlinBaseCopying) <NSCopying>
+@end;
+
+__attribute__((objc_runtime_name("KotlinMutableSet")))
+__attribute__((swift_name("KotlinMutableSet")))
+@interface SharedMutableSet<ObjectType> : NSMutableSet<ObjectType>
+@end;
+
+__attribute__((objc_runtime_name("KotlinMutableDictionary")))
+__attribute__((swift_name("KotlinMutableDictionary")))
+@interface SharedMutableDictionary<KeyType, ObjectType> : NSMutableDictionary<KeyType, ObjectType>
+@end;
+
+@interface NSError (NSErrorKotlinException)
+@property (readonly) id _Nullable kotlinException;
+@end;
+
+__attribute__((objc_runtime_name("KotlinNumber")))
+__attribute__((swift_name("KotlinNumber")))
+@interface SharedNumber : NSNumber
+- (instancetype)initWithChar:(char)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedChar:(unsigned char)value __attribute__((unavailable));
+- (instancetype)initWithShort:(short)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedShort:(unsigned short)value __attribute__((unavailable));
+- (instancetype)initWithInt:(int)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedInt:(unsigned int)value __attribute__((unavailable));
+- (instancetype)initWithLong:(long)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedLong:(unsigned long)value __attribute__((unavailable));
+- (instancetype)initWithLongLong:(long long)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedLongLong:(unsigned long long)value __attribute__((unavailable));
+- (instancetype)initWithFloat:(float)value __attribute__((unavailable));
+- (instancetype)initWithDouble:(double)value __attribute__((unavailable));
+- (instancetype)initWithBool:(BOOL)value __attribute__((unavailable));
+- (instancetype)initWithInteger:(NSInteger)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedInteger:(NSUInteger)value __attribute__((unavailable));
++ (instancetype)numberWithChar:(char)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedChar:(unsigned char)value __attribute__((unavailable));
++ (instancetype)numberWithShort:(short)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedShort:(unsigned short)value __attribute__((unavailable));
++ (instancetype)numberWithInt:(int)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedInt:(unsigned int)value __attribute__((unavailable));
++ (instancetype)numberWithLong:(long)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedLong:(unsigned long)value __attribute__((unavailable));
++ (instancetype)numberWithLongLong:(long long)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedLongLong:(unsigned long long)value __attribute__((unavailable));
++ (instancetype)numberWithFloat:(float)value __attribute__((unavailable));
++ (instancetype)numberWithDouble:(double)value __attribute__((unavailable));
++ (instancetype)numberWithBool:(BOOL)value __attribute__((unavailable));
++ (instancetype)numberWithInteger:(NSInteger)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedInteger:(NSUInteger)value __attribute__((unavailable));
+@end;
+
+__attribute__((objc_runtime_name("KotlinByte")))
+__attribute__((swift_name("KotlinByte")))
+@interface SharedByte : SharedNumber
+- (instancetype)initWithChar:(char)value;
++ (instancetype)numberWithChar:(char)value;
+@end;
+
+__attribute__((objc_runtime_name("KotlinUByte")))
+__attribute__((swift_name("KotlinUByte")))
+@interface SharedUByte : SharedNumber
+- (instancetype)initWithUnsignedChar:(unsigned char)value;
++ (instancetype)numberWithUnsignedChar:(unsigned char)value;
+@end;
+
+__attribute__((objc_runtime_name("KotlinShort")))
+__attribute__((swift_name("KotlinShort")))
+@interface SharedShort : SharedNumber
+- (instancetype)initWithShort:(short)value;
++ (instancetype)numberWithShort:(short)value;
+@end;
+
+__attribute__((objc_runtime_name("KotlinUShort")))
+__attribute__((swift_name("KotlinUShort")))
+@interface SharedUShort : SharedNumber
+- (instancetype)initWithUnsignedShort:(unsigned short)value;
++ (instancetype)numberWithUnsignedShort:(unsigned short)value;
+@end;
+
+__attribute__((objc_runtime_name("KotlinInt")))
+__attribute__((swift_name("KotlinInt")))
+@interface SharedInt : SharedNumber
+- (instancetype)initWithInt:(int)value;
++ (instancetype)numberWithInt:(int)value;
+@end;
+
+__attribute__((objc_runtime_name("KotlinUInt")))
+__attribute__((swift_name("KotlinUInt")))
+@interface SharedUInt : SharedNumber
+- (instancetype)initWithUnsignedInt:(unsigned int)value;
++ (instancetype)numberWithUnsignedInt:(unsigned int)value;
+@end;
+
+__attribute__((objc_runtime_name("KotlinLong")))
+__attribute__((swift_name("KotlinLong")))
+@interface SharedLong : SharedNumber
+- (instancetype)initWithLongLong:(long long)value;
++ (instancetype)numberWithLongLong:(long long)value;
+@end;
+
+__attribute__((objc_runtime_name("KotlinULong")))
+__attribute__((swift_name("KotlinULong")))
+@interface SharedULong : SharedNumber
+- (instancetype)initWithUnsignedLongLong:(unsigned long long)value;
++ (instancetype)numberWithUnsignedLongLong:(unsigned long long)value;
+@end;
+
+__attribute__((objc_runtime_name("KotlinFloat")))
+__attribute__((swift_name("KotlinFloat")))
+@interface SharedFloat : SharedNumber
+- (instancetype)initWithFloat:(float)value;
++ (instancetype)numberWithFloat:(float)value;
+@end;
+
+__attribute__((objc_runtime_name("KotlinDouble")))
+__attribute__((swift_name("KotlinDouble")))
+@interface SharedDouble : SharedNumber
+- (instancetype)initWithDouble:(double)value;
++ (instancetype)numberWithDouble:(double)value;
+@end;
+
+__attribute__((objc_runtime_name("KotlinBoolean")))
+__attribute__((swift_name("KotlinBoolean")))
+@interface SharedBoolean : SharedNumber
+- (instancetype)initWithBool:(BOOL)value;
++ (instancetype)numberWithBool:(BOOL)value;
+@end;
+
+__attribute__((objc_subclassing_restricted))
+__attribute__((swift_name("User")))
+@interface SharedUser : KotlinBase
+- (instancetype)initWithId:(NSString * _Nullable)id name:(NSString * _Nullable)name dob:(NSString * _Nullable)dob phoneNumber:(NSString * _Nullable)phoneNumber __attribute__((swift_name("init(id:name:dob:phoneNumber:)"))) __attribute__((objc_designated_initializer));
+- (NSString *)toText __attribute__((swift_name("toText()")));
+- (NSString * _Nullable)component1 __attribute__((swift_name("component1()")));
+- (NSString * _Nullable)component2 __attribute__((swift_name("component2()")));
+- (NSString * _Nullable)component3 __attribute__((swift_name("component3()")));
+- (NSString * _Nullable)component4 __attribute__((swift_name("component4()")));
+- (SharedUser *)doCopyId:(NSString * _Nullable)id name:(NSString * _Nullable)name dob:(NSString * _Nullable)dob phoneNumber:(NSString * _Nullable)phoneNumber __attribute__((swift_name("doCopy(id:name:dob:phoneNumber:)")));
+@property (readonly) NSString * _Nullable id;
+@property (readonly) NSString * _Nullable name;
+@property (readonly) NSString * _Nullable dob;
+@property (readonly) NSString * _Nullable phoneNumber;
+@end;
+
+__attribute__((objc_subclassing_restricted))
+__attribute__((swift_name("VideoGame")))
+@interface SharedVideoGame : KotlinBase
+- (instancetype)initWithName:(NSString * _Nullable)name publisher:(NSString * _Nullable)publisher reviewScore:(int32_t)reviewScore __attribute__((swift_name("init(name:publisher:reviewScore:)"))) __attribute__((objc_designated_initializer));
+- (NSString *)toText __attribute__((swift_name("toText()")));
+- (NSString * _Nullable)component1 __attribute__((swift_name("component1()")));
+- (NSString * _Nullable)component2 __attribute__((swift_name("component2()")));
+- (int32_t)component3 __attribute__((swift_name("component3()")));
+- (SharedVideoGame *)doCopyName:(NSString * _Nullable)name publisher:(NSString * _Nullable)publisher reviewScore:(int32_t)reviewScore __attribute__((swift_name("doCopy(name:publisher:reviewScore:)")));
+@property (readonly) NSString * _Nullable name;
+@property (readonly) NSString * _Nullable publisher;
+@property (readonly) int32_t reviewScore;
+@end;
+
+__attribute__((objc_subclassing_restricted))
+__attribute__((swift_name("CommonKt")))
+@interface SharedCommonKt : KotlinBase
++ (void)helloWorld __attribute__((swift_name("helloWorld()")));
+@end;
+
+NS_ASSUME_NONNULL_END
